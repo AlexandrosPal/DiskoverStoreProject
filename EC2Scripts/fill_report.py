e9a7021a88884f67ab8d9954a3a6f3f942050b32
apaliakoudis <paliakoudis.alex@gmail.com>
2023-12-25 12:29:23 +0200

    Rebuilt fill_report.py

--- EC2Scripts/fill_report.py
+++ EC2Scripts/fill_report.py
@@ -1,10 +1,9 @@
-import aspose.pdf as pdf
+from reportlab.lib.pagesizes import A4, landscape
+from reportlab.pdfgen import canvas
 from datetime import datetime as dt
 from datetime import timedelta
-import sys
 
-def fill_report():
-    # Date and File variables
+def add_image_to_existing_pdf():
     dateFrom = (dt.now() - timedelta(days=7)).strftime('%Y/%m/%d')
     dateTo = dt.now().strftime('%Y/%m/%d')
     dateString = f'{dateFrom} - {dateTo}'
@@ -13,57 +12,44 @@ def fill_report():
     file_dateTo = dt.now().strftime('%Y%m%d')
     fileName = f'report_{file_dateFrom}-{file_dateTo}.pdf'
 
-    # Files
-    sys.path.append("/home/ec2-user/DiskoverProject")
-    document = pdf.Document('assets/report_blueprint.pdf')
-    stamp_most_sold = pdf.ImageStamp('assets/most_sold.png')
-    stamp_most_orders = pdf.ImageStamp('assets/most_orders.png')
-    stamp_revenues_generated = pdf.ImageStamp('assets/revenues_generated.png')
-    stamp_most_busy_dates = pdf.ImageStamp('assets/most_busy_dates.png')
+    pdf = canvas.Canvas(f'/DiskoverProject/assets/{fileName}', pagesize=landscape(A4))
 
-    # Date properties
-    dateField = pdf.TextStamp(dateString)
-    dateField.x_indent = 135
-    dateField.y_indent = 528
+    tl_image = '/DiskoverProject/assets/most_sold.png'
+    tr_image = '/DiskoverProject/assets/most_orders.png'
+    bl_image = '/DiskoverProject/assets/revenues_generated.png'
+    br_image = '/DiskoverProject/assets/most_busy_dates.png'
 
-    # Stamp_most_sold properties
-    stamp_most_sold.x_indent = 60
-    stamp_most_sold.y_indent = 255
-    stamp_most_sold.width = 300
-    stamp_most_sold.height = 220
+    image_width = 270
+    image_height = 200
 
-    # Stamp_most_orders properties
-    stamp_most_orders.x_indent = 60
-    stamp_most_orders.y_indent = 10
-    stamp_most_orders.width = 300
-    stamp_most_orders.height = 220
+    x_indent = 50
+ 
+    # Title
+    pdf.setFont('Helvetica', 20)
+    pdf.drawString(x_indent, 550, 'Diskover Disk Store Report')
+    pdf.setFont('Helvetica', 14)
+    pdf.drawString(x_indent, 530, 'Time period:')
 
-    # Stamp_revenue_generated properties
-    stamp_revenues_generated.x_indent = 450
-    stamp_revenues_generated.y_indent = 255
-    stamp_revenues_generated.width = 300
-    stamp_revenues_generated.height = 220
+    pdf.drawString(133, 529.5, dateString)
 
-    # Stamp_most_busy_dates properties
-    stamp_most_busy_dates.x_indent = 450
-    stamp_most_busy_dates.y_indent = 10
-    stamp_most_busy_dates.width = 300
-    stamp_most_busy_dates.height = 220
+    # Top left
+    pdf.drawString(x_indent, 490, 'Most sold products:')
+    pdf.drawImage(tl_image, x_indent, 480-image_height, width=image_width, height=image_height)
 
-    # Page
-    page = document.pages[1]
+    # Top right
+    pdf.drawString(450, 490, 'Most sold products:')
+    pdf.drawImage(tr_image, 450, 480-image_height, width=image_width, height=image_height)
 
-    # Add to page
-    page.add_stamp(stamp_most_sold)
-    page.add_stamp(stamp_most_orders)
-    page.add_stamp(stamp_revenues_generated)
-    page.add_stamp(stamp_most_busy_dates)
-    page.add_stamp(dateField)
+    # Bottom left
+    pdf.drawString(x_indent, 250, 'Most sold products:')
+    pdf.drawImage(bl_image, x_indent, 240-image_height, width=image_width, height=image_height)
 
-    # Flatten page
-    page.flatten()
+    # Bottom right
+    pdf.drawString(450, 250, 'Most sold products:')
+    pdf.drawImage(br_image, 450, 240-image_height, width=image_width, height=image_height)
 
-    # Save PDF
-    document.save(f'assets/{fileName}')
 
-    return fileName
+    pdf.save()
+        
+
+add_image_to_existing_pdf()
